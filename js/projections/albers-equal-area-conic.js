be182cfe712f7dbec3cf915587d65315bc8930be
Ben Hackett <benhackett4@gmail.com>
2023-07-02 22:37:18 -0400

    Added useful reference link as a comment

--- js/projections/albers-equal-area-conic.js
+++ js/projections/albers-equal-area-conic.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// https://surferhelp.goldensoftware.com/projections/Albers_Equal_Area_Conic_Projection.htm
+
 function albersEqualAreaConicProjection(coordinates_lists, max_width, max_height) {
 
 
